3c444303642df3c973096c9a2341a66d12edb060
Nikita <62812039+robotics-kid@users.noreply.github.com>
2021-03-17 16:39:16 +0200

    Added: Connection to existed network
    All ESP tries connect to one main ESP(main ESP - first powered on controller)

--- main/main.ino
+++ main/main.ino
@@ -10,31 +10,51 @@
 #include "StripManage.h"
 
 static DNSServer DNS;
+WiFiClient client;
 
 static std::vector<AsyncClient*> clients; // a list to hold all clients
 
 char recv[argsLen];
 char mSend[argsLen];
 bool RequestRecieved = true;
+bool WiFiConnection = false;
 
 void setup() {
   Serial.begin(115200);
   delay(20);
 
-  /*WiFi SetUp*/
-  // create access point
-  while (!WiFi.softAP(SERVER_HOST_NAME, PASSWORD, 6, false, MAX_CLIENTS)) {
-    delay(500);
+  /*Try connection to existed network*/
+  if ( WiFi.begin(SERVER_HOST_NAME, PASSWORD) == WL_CONNECTED) {
+
+    Serial.println("Connected to wifi");
+    Serial.println("\nStarting connection...");
+
+    // if you get a connection, report back via serial:
+    if (client.connect(server, TCP_PORT)) {
+      Serial.println("connected");
+      //      // Make a HTTP request:
+      //      client.println("GET /search?q=arduino HTTP/1.0");
+      //      client.println();
+      //      client.readStringUntil('!');
+      WiFiConnection = true;
+    }
   }
+  else {
 
-  // start dns server
-  if (!DNS.start(DNS_PORT, SERVER_HOST_NAME, WiFi.softAPIP()))
-    Serial.printf("\n Failed to start DNS service \n");
+    /*WiFi SetUp*/
+    // create access point
+    while (!WiFi.softAP(SERVER_HOST_NAME, PASSWORD, 6, false, MAX_CLIENTS)) {
+      delay(500);
+    }
 
-  AsyncServer* server = new AsyncServer(TCP_PORT); // start listening on tcp port 7050
-  server->onClient(&handleNewClient, server);
-  server->begin();
+    // start dns server
+    if (!DNS.start(DNS_PORT, SERVER_HOST_NAME, WiFi.softAPIP()))
+      Serial.printf("\n Failed to start DNS service \n");
 
+    AsyncServer* server = new AsyncServer(TCP_PORT); // start listening on tcp port 7050
+    server->onClient(&handleNewClient, server);
+    server->begin();
+  }
   /* Initializing leds. Based on LEDS TYPE */
 #if LEDS_TYPE == 1
   FastLED.addLeds<WS2812B, DATA_PIN, COLOR_ORDER>(ledsRGB, getRGBWsize(NUM_LEDS)); // Set up for SK6812
